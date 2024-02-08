6c5eb09a81bf93f7195627fb275d0ddb4124e81a
Hector Salas Inostroza <hectorsalasinostroza@MacBook-Pro-de-Hector.local>
2024-02-08 17:47:50 -0300

    add pagination

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
